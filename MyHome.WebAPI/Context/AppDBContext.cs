47b350b460394ec60d507a7a28654809f350b6df
kamalakar141181@gmail.com <kamalakar141181@gmail.com>
2023-04-03 23:59:39 +0530

    Entity Framework Configured

---
+++ MyHome.WebAPI/Context/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyHome.WebAPI.Models;
+using System.Collections.Generic;
+
+namespace MyHome.WebAPI.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext()
+        {
+        }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+           : base(options)
+        {
+
+        }
+        public DbSet<UserEntity> Users { get; set; }
+
+    }
+}
--- MyHome.WebAPI/Program.cs
+++ MyHome.WebAPI/Program.cs
@@ -1,14 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+// Microsoft.EntityFrameworkCore.SqlServer ( Tools -> Nuget Package Manager -> Manage Nuget Packages For Solutions -> Microsoft.EntityFrameworkCore.SqlServer search and install)
+builder.Services.AddDbContext<DbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection")));
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -17,9 +16,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
